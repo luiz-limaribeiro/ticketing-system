26007588304557c00a89a768a060186a0bd4ae2c
luiz-limaribeiro <de.limaribeiroluiz@gmail.com>
2025-11-05 20:55:31 -0300

    feat: create the user screen

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import UserScreen from './components/UserScreen'
+import './App.css'
+
+function App() {
+  return (
+    <UserScreen />
+  )
+}
+
+export default App
---
+++ web/src/components/SuccessModal.tsx
@@ -0,0 +1,13 @@
+import './styles/SuccessModal.css'
+
+export default function SuccessModal() {
+  return (
+    <div>
+      <div>success</div>
+      <span>Success!</span>
+      <span>Your ticket has been submitted</span>
+      <span>Ticket ID: ...</span>
+      <p>An email has been sent...</p>
+    </div>
+  )
+}
